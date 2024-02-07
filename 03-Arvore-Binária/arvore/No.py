7d6b3e4e13639eb0831b8429e5cc8e8d509a71d5
Silas Silva da Costa <silllas22@gmail.com>
2024-02-07 17:49:21 -0300

    ajuste projeto da arvore binaria

--- 03-Arvore-Binária/arvore/No.py
+++ 03-Arvore-Binária/arvore/No.py
@@ -1,12 +1,12 @@
 class No(object):
     
-    def __init__(self, objeto : object) -> None:
+    def __init__(self, dado : object) -> None:
         """
             Construtor da classe Nó
 
             Args:
                 objeto (object): Objeto a ser colocado no nó
         """
-        self.objeto : object = objeto
+        self.dado : object = dado
         self.direita : No = None
         self.esquerda : No = None
